318f231cc5de1a190f6ae4a5b213101bdb05a849
Tejas Tupe <93927655+Tejasblogs@users.noreply.github.com>
2023-06-25 23:31:06 +0530

    Add files via upload

---
+++ Swap1.java
@@ -0,0 +1,19 @@
+class Swap1 
+{
+	public static void main(String[] args) 
+	{
+		// Swap Two Numbers with 3rd variable
+
+int a = 45;
+int b = 78;
+
+int c = a;
+	a = b;
+	b = c;
+
+
+System.out.println("a "+a);
+System.out.println("b "+b);
+
+	}
+}
