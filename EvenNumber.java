431243a2787418efaf511f292d7e2a3caaaeec6b
Tejas Tupe <93927655+Tejasblogs@users.noreply.github.com>
2023-06-25 23:36:57 +0530

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,20 @@
+class EvenNumber 
+{
+	public static void main(String[] args) 
+	{
+		// print Even number for this = 1-50;
+
+				int a = 1;
+				
+			
+	
+     for ( int b = 1;b<=50 ;b++ )
+			{
+			 if (a%2==0)
+			 {
+               System.out.print(" "+a+" ");
+			 }
+             a++;
+			}
+	}
+}
