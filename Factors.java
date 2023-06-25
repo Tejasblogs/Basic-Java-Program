333897c30163d2104cd8018d19ea7b6d3df519e7
Tejas Tupe <93927655+Tejasblogs@users.noreply.github.com>
2023-06-25 23:43:04 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,21 @@
+class Factors 
+{
+	public static void main(String[] args) 
+	{
+		// Factorial of 45
+
+        int a = 45;
+	System.out.print( "Factorial of 45 are following >>>");	
+    
+    for (int b=1 ; b<=45 ; b++ )
+       {  
+
+          if (a%b==00)
+             {
+                  System.out.print(" "+b+" ");
+             }
+ 
+       }
+
+	}
+}
